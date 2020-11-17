4c03c3700485ee0772e07fa17f0c59be628346d5
allan Maubert <a.maubert.jouvet@gmail.com>
2020-11-17 12:22:12 +0100

    initial commit

---
+++ contracts/Poll.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Poll {
+  address public owner = msg.sender;
+  uint public last_completed_migration;
+
+  modifier restricted() {
+    require(
+      msg.sender == owner,
+      "This function is restricted to the contract's owner"
+    );
+    _;
+  }
+
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
+
+  function pingTest() public pure returns (string memory) {
+    string memory pong =  "Pong";
+    return pong;
+  }
+}
---
+++ test/ping.test.spec.js
@@ -0,0 +1,19 @@
+const Poll = artifacts.require('Poll');
+
+contract('Poll contract', (accounts) => {
+    let [alice, bob] = accounts;
+    let contractInstance ;
+
+    beforeEach( async () => {
+        contractInstance = await Poll.new();
+    })
+
+    it('Should return pong', async () => {
+
+        const result = await contractInstance.pingTest();
+        assert.equal(result, 'Pong');
+    })
+})
+
+
+
