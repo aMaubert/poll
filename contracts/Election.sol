ec7584dc3e9b36609d996afd7d3d837632492b86
ValentinGourdon <valentin.gourdon@gmail.com>
2020-11-17 20:16:57 +0100

    Added Election.sol and Candidate.sol

---
+++ contracts/Election.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election {
+    address public owner = msg.sender;
+    uint public last_completed_migration;
+
+    string name;
+
+
+    modifier restricted() {
+        require(
+            msg.sender == owner,
+            "This function is restricted to the contract's owner"
+        );
+        _;
+    }
+
+    function setCompleted(uint completed) public restricted {
+        last_completed_migration = completed;
+    }
+}
