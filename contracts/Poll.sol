359a99ba8cafee4df79237731c6a87de150ba44d
ValentinGourdon <valentin.gourdon@gmail.com>
2020-11-27 19:08:01 +0100

    Add is Candidate to Poll

--- contracts/Poll.sol
+++ contracts/Poll.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.8.0;
 import "./Election.sol";
 import "./CalculUtils.sol";
 
-contract Poll is Election, Vote, CalculUtils{
+contract Poll is Election, Vote, CalculUtils, Candidate {
   address public owner = msg.sender;
   uint public last_completed_migration;
 
